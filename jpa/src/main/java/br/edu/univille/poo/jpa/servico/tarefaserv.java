f5a0011278139a37739fa5a1e23ec53b9e9c397b
mariaeduardaOS <83980857+mariaeduardaOS@users.noreply.github.com>
2024-09-05 19:37:59 -0300

    Update and rename TarefaService.java to tarefaserv.java

--- jpa/src/main/java/br/edu/univille/poo/jpa/servico/tarefaserv.java
+++ jpa/src/main/java/br/edu/univille/poo/jpa/servico/tarefaserv.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class TarefaService {
+public class tarefaserv {
 
     @Autowired
     private TarefaRepository tarefaRepository;
