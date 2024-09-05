533e6d63ae42896d38f05534f4c9c7f91f4238e6
mariaeduardaOS <83980857+mariaeduardaOS@users.noreply.github.com>
2024-09-05 19:42:31 -0300

    Update data.sql

--- jpa/src/main/resources/data.sql
+++ jpa/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into pessoa(id,nome,cpf) values(1,'John Travolta','000.000.000-00');
-insert into pessoa(id,nome,cpf) values(2,'Tom Cruise ','123.456.789-01');
+insert into pessoa(id,nome,cpf) values(1,'Carla','000.000.000-00');
+insert into pessoa(id,nome,cpf) values(2,'Luiz ','123.456.789-01');
